b5db9dc6f7603a82f5d2f124fec94286e0f1a325
DianaMun <diana@nsuna.com>
2025-03-03 11:19:22 +0200

    Create, View and Edit APIs running

--- backend/app/models.py
+++
@@ -1,8 +0,0 @@
-from .db import db
-
-class Transaction(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    amount = db.Column(db.Numeric, nullable=False)
-    date = db.Column(db.DateTime, nullable=False)
-    type = db.Column(db.String(10), nullable=False)  # "credit" or "debit"
-    description = db.Column(db.Text, nullable=False)
